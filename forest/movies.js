44a244fd27b9e6733119f3554313690fe30e993d
Philippe Gadiou <philippe.gadiou@gmail.com>
2020-04-14 20:22:42 +0200

    woodshop examples

--- forest/movies.js
+++ forest/movies.js
@@ -1,15 +1,80 @@
 const { collection } = require('forest-express-sequelize');
 const sequelize = require('sequelize');
+const models = require('../models');
 
 const { Op } = sequelize;
 // const models = require('../models');
 
 collection('movies', {
   actions: [{
-    name: 'send info',
+    name: 'update movie characteristics',
+    type: 'single',
+    fields: [{
+      field: 'language',
+      type: 'Boolean',
+      description: 'insert value to update field',
+    }, {
+      field: 'gore',
+      type: 'Boolean',
+      description: 'insert value to update field',
+    }, {
+      field: 'drugs',
+      type: 'Boolean',
+      description: 'insert value to update field',
+    }, {
+      field: 'graphicViolence',
+      type: 'Boolean',
+      description: 'insert value to update field',
+    }, {
+      field: 'nudity',
+      type: 'Boolean',
+      description: 'insert value to update field',
+    }],
+    values: async (context) => {
+      console.log(context);
+      // wait until you fetch the movie record - do not forget to include the movie characteristics model
+      const movie = await models.movies.findByPk(context.id, { include: [{ model: models.movieCharacteristics, as: 'movieCharacteristic' }] });
+      console.log(movie);
+      // Forest will automatically match all of the form fields that have the same name as your movie characteristics fields and prefill with their value
+      return movie.movieCharacteristic;
+    },
   }],
   fields: [
     {
+      field: 'characteristics',
+      type:  'String',
+      get: (movie) => {
+        //check if the movie has a related characteristics record to return smtg or not
+        if (movie.movieCharacteristic) {
+          // list all your fields from the movieCharacteristics collection you want to display
+          const characteristicsNameList = ['language', 'gore', 'drugs', 'graphicViolence', 'nudity'];
+          // create empty string which will be filled with a div per field listed above - this string will be the value returned
+          let characteristicsList = '';
+          // add style that will be used to display the movie_characteristics info
+          const characteristicsDivStyle = 'margin: 24px 0px; color: #415574'
+          const characteristicsNameStyle = 'padding: 6px 16px; margin: 12px; background-color:#b5c8d05e; border-radius: 6px'
+          const characteristicsValueStyleRed = 'padding: 6px 12px; background-color:#ff7f7f87; border-radius: 6px'
+          const characteristicsValueStyleGreen = 'padding: 6px 12px; background-color:#7FFF7F; border-radius: 6px'
+          // iterate over the list of movie characteristics fields
+          for (index = 0; index < characteristicsNameList.length; index++) {
+            const fieldName = characteristicsNameList[index];
+            // check if the value of the field is 0 or 1 to add the relevant style (default is 0)
+            let characteristicsValueStyle = characteristicsValueStyleRed;
+            console.log(fieldName, movie.movieCharacteristic[fieldName])
+            if (movie.movieCharacteristic[fieldName] === true) {
+              console.log('1 detected')
+              characteristicsValueStyle = characteristicsValueStyleGreen;
+            }
+            // insert the div with the field info to the string that will be returned
+            characteristicsList += `<div style="${characteristicsDivStyle}">
+                <span style="${characteristicsNameStyle}">${fieldName}</span>
+                <span style="${characteristicsValueStyle}">${movie.movieCharacteristic[fieldName]}</span>
+              </div>`;
+          }
+          return characteristicsList;
+        }
+      }
+    },{
       field: 'Director name',
       type: 'String',
       get(movie) {
@@ -60,7 +125,7 @@ collection('movies', {
     //       //   ],
     //       // },
     //     });
-      
+
     //     // TRY USING SEQUELIZE LITERAL
     //     // const searchCondition = models.sequelize.literal(`roles.name ILIKE '%${search}%'`);
 
--- routes/movie-characteristics.js
+++ routes/movie-characteristics.js
@@ -1,52 +1,48 @@
 const express = require('express');
 const { PermissionMiddlewareCreator } = require('forest-express-sequelize');
-const { movie_characteristics } = require('../models');
+const { movieCharacteristics } = require('../models');
 
 const router = express.Router();
-const permissionMiddlewareCreator = new PermissionMiddlewareCreator('movie_characteristics');
-
-// This file contains the logic of every route in Forest Admin for the collection movie_characteristics:
-// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/extend-a-route
-// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/actions/create-and-manage-smart-actions
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('movieCharacteristics');
 
 // Create a Movie Characteristic
-router.post('/movie_characteristics', permissionMiddlewareCreator.create(), (request, response, next) => {
+router.post('/movieCharacteristics', permissionMiddlewareCreator.create(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#create-a-record
   next();
 });
 
 // Update a Movie Characteristic
-router.put('/movie_characteristics/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+router.put('/movieCharacteristics/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#update-a-record
   next();
 });
 
 // Delete a Movie Characteristic
-router.delete('/movie_characteristics/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+router.delete('/movieCharacteristics/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#delete-a-record
   next();
 });
 
 // Get a list of Movie Characteristics
-router.get('/movie_characteristics', permissionMiddlewareCreator.list(), (request, response, next) => {
+router.get('/movieCharacteristics', permissionMiddlewareCreator.list(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-list-of-records
   next();
 });
 
 // Get a number of Movie Characteristics
-router.get('/movie_characteristics/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+router.get('/movieCharacteristics/count', permissionMiddlewareCreator.list(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-number-of-records
   next();
 });
 
 // Get a Movie Characteristic
-router.get('/movie_characteristics/:recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+router.get('/movieCharacteristics/:recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-record
   next();
 });
 
 // Export a list of Movie Characteristics
-router.get('/movie_characteristics.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+router.get('/movieCharacteristics.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
   // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#export-a-list-of-records
   next();
 });
